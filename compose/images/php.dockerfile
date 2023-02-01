0b4b11cf254d8556fbd13d442ba9ea8e8dc3db64
Talgat Hairullov <thairullov@cs-cart.com>
2023-02-02 00:34:40 +0400

    [~] {1-32391} Refactored to interact with node server.

--- compose/images/php.dockerfile
+++ compose/images/php.dockerfile
@@ -4,6 +4,9 @@ ARG PROJECT=pdf
 ARG PROJECT_HOME=/var/www/html/genworker
 #
 COPY ${PROJECT} ${PROJECT_HOME}
+COPY supervisord.conf /etc/supervisord.conf
+COPY www.conf /usr/local/etc/php-fpm.d/www.conf
+
 #
 WORKDIR ${PROJECT_HOME}/app
 RUN mkdir -m777 ${PROJECT_HOME}/app/db \
--- compose/images/php/garbage_cleaner.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-find /tmp/ -mmin +60 -name "php*" | xargs --no-run-if-empty rm
-find /var/www/html/genworker/app/files -mmin +60 -name "*.pdf" | xargs --no-run-if-empty rm
-find /var/www/html/genworker/app/files -mmin +60 -name "*.html" | xargs --no-run-if-empty rm
-sleep 60
